8344d52fb2b3a7ff14869a5068a3664a46d71adb
karichalobato <00002517@uca.edu.sv>
2019-05-15 12:02:09 -0600

    Add a Room database

--- app/src/main/java/com/Lobato/roomwordsample/data/WordRoomDatabase.kt
+++ app/src/main/java/com/Lobato/roomwordsample/data/WordRoomDatabase.kt
@@ -1,4 +1,35 @@
 package com.Lobato.roomwordsample.data
 
-class WordRoomDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.Lobato.roomwordsample.data.dao.WordDao
+import com.Lobato.roomwordsample.data.models.Word
+import java.security.AccessControlContext
+
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDa(): WordDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "Word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
