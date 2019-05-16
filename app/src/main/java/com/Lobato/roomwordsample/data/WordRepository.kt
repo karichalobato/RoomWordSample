c6f6ca51bab512301a6f6a68f0a178b500e8ffa5
karichalobato <00002517@uca.edu.sv>
2019-05-15 19:02:22 -0600

    Create the Repository

--- app/src/main/java/com/Lobato/roomwordsample/data/WordRepository.kt
+++ app/src/main/java/com/Lobato/roomwordsample/data/WordRepository.kt
@@ -1,4 +1,16 @@
 package com.Lobato.roomwordsample.data
 
-class WordRepository {
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import com.Lobato.roomwordsample.data.dao.WordDao
+import com.Lobato.roomwordsample.data.models.Word
+
+class WordRepository (private val wordDao: WordDao){
+
+    val allWords: LiveData<List<Word>> = wordDao.getAllWords()
+
+    @WorkerThread
+    suspend fun insert(word: Word){
+        wordDao.insert(word)
+    }
 }
